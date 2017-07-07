aebbd49eeb20feb3ca8941ab4f5c043d1c61f7df
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-07-06 22:51:46 -0300

    it shall be a http server

--- main.go
+++ main.go
@@ -7,54 +7,111 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	chart "github.com/wcharczuk/go-chart"
 )
 
 const pageSize = 100
 
-var (
-	repo  string
-	token string
-)
+var token string
 
 type stargazer struct {
-	StarredAt string `json:"starred_at"`
+	StarredAt time.Time `json:"starred_at"`
+}
+
+type repository struct {
+	FullName    string `json:"full_name"`
+	Permissions struct {
+		Push bool
+	}
 }
 
 func main() {
 	token = os.Getenv("GITHUB_TOKEN")
-	repo = os.Args[1]
-	if !strings.Contains(repo, "/") {
-		log.Fatalln("you need to pass a repo in the owner/name format")
-	}
-	var page = 1
-	for {
-		url := fmt.Sprintf(
-			"https://api.github.com/repos/%s/stargazers?page=%d&per_page=%d",
-			repo, page, pageSize,
-		)
-		req, err := http.NewRequest(http.MethodGet, url, nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		repo, err := getRepo(r.URL.Path[1:])
 		if err != nil {
-			log.Fatalln(err)
-		}
-		req.Header.Add("Accept", "application/vnd.github.v3.star+json")
-		if token != "" {
-			req.Header.Add("Authorization", "token "+token)
-		}
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer resp.Body.Close()
-		var stargazers []stargazer
-		if err := json.NewDecoder(resp.Body).Decode(&stargazers); err != nil {
-			log.Fatalln(err)
-		}
-		if len(stargazers) == 0 {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		for i, star := range stargazers {
-			fmt.Printf("%v\t%v\n", i+((page-1)*pageSize), star.StarredAt)
+		if !repo.Permissions.Push {
+			http.Error(w, "I do not have push permissions in this repo, won't spend my rate limit with it", http.StatusNotAcceptable)
+			return
 		}
-		page++
+
+		var series = chart.TimeSeries{}
+		var page = 1
+		for {
+			url := fmt.Sprintf(
+				"https://api.github.com/repos/%s/stargazers?page=%d&per_page=%d",
+				repo.FullName, page, pageSize,
+			)
+			req, err := http.NewRequest(http.MethodGet, url, nil)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			req.Header.Add("Accept", "application/vnd.github.v3.star+json")
+			if token != "" {
+				req.Header.Add("Authorization", "token "+token)
+			}
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer resp.Body.Close()
+			var stargazers []stargazer
+			if err := json.NewDecoder(resp.Body).Decode(&stargazers); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if len(stargazers) == 0 {
+				break
+			}
+			for i, star := range stargazers {
+				series.XValues = append(series.XValues, star.StarredAt)
+				series.YValues = append(series.YValues, float64(i+((page-1)*pageSize)))
+			}
+			page++
+		}
+		graph := chart.Chart{
+			XAxis: chart.XAxis{
+				Name:      "Time",
+				NameStyle: chart.StyleShow(),
+				Style:     chart.StyleShow(),
+			},
+			YAxis: chart.YAxis{
+				Name:      "Sargazers",
+				NameStyle: chart.StyleShow(),
+				Style:     chart.StyleShow(),
+			},
+			Series: []chart.Series{series},
+		}
+		w.Header().Add("Content-Type", "image/svg+xml")
+		graph.Render(chart.SVG, w)
+	})
+	log.Fatalln(http.ListenAndServe(":3000", nil))
+}
+
+func getRepo(name string) (repo repository, err error) {
+	if !strings.Contains(name, "/") {
+		return repo, fmt.Errorf("invalid repo: %v", name)
 	}
+	url := fmt.Sprintf("https://api.github.com/repos/%s", name)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return
+	}
+	if token != "" {
+		req.Header.Add("Authorization", "token "+token)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&repo)
+	return
 }
