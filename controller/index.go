0fa49779b1a3ccd2f0ddef65af3276c2411c6146
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-05-02 08:35:32 -0300

    clean: debug code

--- controller/index.go
+++ controller/index.go
@@ -2,27 +2,11 @@ package controller
 
 import (
 	"html/template"
-	"io/ioutil"
 	"net/http"
-	"os"
-
-	"github.com/apex/log"
 )
 
 func Index() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		dir, err := os.Getwd()
-		if err != nil {
-			log.WithError(err).Error("failed to read dir")
-		}
-		log.WithField("cwd", dir).Info("current dir")
-		files, err := ioutil.ReadDir(dir)
-		if err != nil {
-			log.WithError(err).Error("failed to read dir")
-		}
-		for _, f := range files {
-			log.Info(f.Name())
-		}
 		template.Must(template.ParseFiles("templates/index.html")).Execute(w, nil)
 	}
 }
