86ef50173a8f318d84c018c39774528718df2672
Hüseyin Bera Bulut <45367283+berabulut@users.noreply.github.com>
2021-08-01 00:45:02 +0300

    feat: form for searching repo
    * feat: form for searching repo
    
    * feat: style search form

--- controller/index.go
+++ controller/index.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"html/template"
 	"io/fs"
 	"net/http"
@@ -13,5 +14,13 @@ func Index(fsys fs.FS) http.HandlerFunc {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
+
+	}
+}
+
+func HandleForm(fsys fs.FS) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := fmt.Sprintf("/%s/%s", r.FormValue("username"), r.FormValue("repository"))
+		http.Redirect(w, r, url, http.StatusSeeOther)
 	}
 }
