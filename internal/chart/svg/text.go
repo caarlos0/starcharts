31d8f93e7564198e8de289d02ed59baab6cd6e2e
Benni_013 <45521012+Benni013@users.noreply.github.com>
2025-03-14 13:28:00 +0100

    feat: Optimize SVG output (#219)
    * fix: remove duplicate tags
    
    Duplicate tags are no longer generated if tags have no content
    
    * fix: remove xmlns attribute in text tags

--- internal/chart/svg/text.go
+++ internal/chart/svg/text.go
@@ -3,8 +3,6 @@ package svg
 func Text() *TagBuilder {
 	return &TagBuilder{
 		tag: "text",
-		attributes: map[string]string{
-			"xmlns": "http://www.w3.org/2000/svg",
-		},
+		attributes: map[string]string{},
 	}
 }
